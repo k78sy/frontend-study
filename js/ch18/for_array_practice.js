ace774adeb71a456f0bcc0cd0782349199a83c11
ksy <tndud4615@gmail.com>
2025-12-19 18:14:00 +0900

    for array practice

--- js/ch18/for_array_practice.js
+++ js/ch18/for_array_practice.js
@@ -1,8 +1,8 @@
-const numbers = [1, 2, 3, 4, 5];
-const newNumber = [];
+const words = ['hello', 'js', 'array', 'loop'];
+const arr = [];
 
-for (let i = 0; i < numbers.length; i++) {
-    newNumber.push(numbers[i]*2);
+for(let i = 0; i < words.length; i++){
+    arr.push(words[i].length);
 }
 
-console.log(newNumber);
+console.log(arr);
