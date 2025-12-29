7dbf69b2ad9725662cd6c03c188ee7d13ef9ed83
ksy <tndud4615@gmail.com>
2025-12-29 11:07:01 +0900

    vue-project

--- vue/vue-project/src/App.vue
+++ vue/vue-project/src/App.vue
@@ -1,85 +1,11 @@
+<!-- 로직 담당 -->
 <script setup>
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+import FrontWheel from '@/components/FrontWheel.vue';
+// . app.vue를 기준으로 자리를 찾아가는 상대주소
+// @ 절대주소에 가까움.  src 기준에서 찾아갖기
 </script>
 
+<!-- 화면 View 담당-->
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header>
-
-  <RouterView />
+  <FrontWheel></FrontWheel>
 </template>
-
-<style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
-}
-</style>
---
+++ vue/vue-project/src/components/FrontWheel.vue
@@ -0,0 +1,18 @@
+<script setup></script>
+<template>
+    <div class="front-wheel">
+        <span>FRONT WHEEL</span>
+    </div>
+</template>
+<style scoped>
+    .front-wheel{
+        border: 1px solid #000;
+        border-radius: 50%;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        width: 150px;
+        height: 150px;
+    }
+</style>
